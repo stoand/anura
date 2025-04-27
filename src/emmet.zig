7b4adb8f67864ff88d0343e0cd548d5d09aa025c
Andreas Stocker <stoand@protonmail.com>
2025-04-27 14:49:50 +0200

    emmet: add auto type generation

--- src/emmet.zig
+++ src/emmet.zig
@@ -1,11 +1,32 @@
 const std = @import("std");
 const testing = std.testing;
 
-fn em(comptime source: []const u8) void {
+fn em(comptime source: []const u8) type {
     _ = source;
+
+    return @Type(.{
+        .@"struct" = .{
+            .layout = .auto,
+            .fields = &.{
+                .{
+                    .alignment = 0,
+                    .default_value_ptr = null,
+                    .is_comptime = false,
+                    .name = "bindthis",
+                    .type = []const u8,
+                },
+            },
+            .decls = &.{},
+            .is_tuple = false,
+        },
+    });
 }
 
 test {
+    const ty = em("div.class0{@bindthis}");
+
+    const val: ty = .{ .bindthis = "asdf" };
+    _ = val;
+    
     try testing.expect(true);
 }
-
